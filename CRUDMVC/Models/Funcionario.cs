c52afe3026573652250b53cb7a0b17aa8982c16f
Marcos-PCastro <courrielprive@yahoo.com.br>
2023-12-26 10:49:03 -0300

    Basic Project CRUD

---
+++ CRUDMVC/Models/Funcionario.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUDMVC.Models
+{
+    [Table("Funcionario")]
+    public class Funcionario
+    {
+        [Column("Id")]
+        [DisplayName("Registro")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [DisplayName("Nome")]
+        public string Nome { get; set; } 
+    }
+}
